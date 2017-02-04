66f99c79bc0b576c65534c077a3772a5e3892e80
Cédric Sauriol <cedric.sauriol@hotmail.com>
2017-02-03 23:27:59 -0500

    first adds to install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
-$DOTFILES_DIR="~/.dotfiles"
+DF_PATH="$HOME/.dotfiles"
 
-# Making synlinks.
-ln -sfv "$DOTFILES_DIR/profile/.prompt"
+if ! command -v "vim" > /dev/null; then
+    echo "Vim is not installed." 
+    echo "Installing Vim"
+    sudo apt-get -y install vim
+else
+    echo "Vim OK!"
+fi
+
+if ! [ -n 'which Java' ]; then
+  echo "Java is not installed."
+  echo "Installing Java"
+  sudo apt-get -y install latest-jre
+  sudo apt-get -y install latest-jdk
+else
+  echo "Java OK!"
+fi
+
+echo "°º¤ø,¸¸,ø¤º°°º¤ø,¸,ø¤º°°º¤ø,¸¸,ø¤º°°º¤ø,¸¸,ø¤º°°º¤ø,¸,ø¤º°°º¤ø,¸¸,ø¤º°"
+echo "Adding dotfiles..."
+cd $DF_PATH
+. ./source.sh
+cd $HOME
